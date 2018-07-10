8d7f7b0c8e3a638a77dd71e17088a2c079338fb6
MartinBG <MartinBG@abv.bg>
2018-07-10 21:06:10 +0300

    Exercise

--- 02. Introduction to DOM and Events/Lab/01. Sum-Numbers/sum-numbers.js
+++ 02. Introduction to DOM and Events/Lab/01. Sum-Numbers/sum-numbers.js
@@ -1,3 +1,6 @@
 function calc() {
-   //TODO: 
+    let num1 = Number(document.getElementById("num1").value);
+    let num2 = Number(document.getElementById("num2").value);
+    document.getElementById("sum").value = num1 + num2;
+    // document.getElementsByTagName("input")[2].value = num1 + num2;
 }
