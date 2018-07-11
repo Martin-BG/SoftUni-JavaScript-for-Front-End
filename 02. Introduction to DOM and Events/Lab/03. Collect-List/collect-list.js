6b291ebc66d7e2a59e5d959c300cce74186c312e
MartinBG <MartinBG@abv.bg>
2018-07-11 15:08:38 +0300

    solution for lab task #3

--- 02. Introduction to DOM and Events/Lab/03. Collect-List/collect-list.js
+++ 02. Introduction to DOM and Events/Lab/03. Collect-List/collect-list.js
@@ -1,3 +1,7 @@
 function extractText() {
-    //TODO:
+    let itemNodes = document.querySelectorAll("ul#items li");
+    let textarea = document.querySelector("#result");
+    for (let node of itemNodes) {
+        textarea.value += node.textContent + "\n";
+    }
 }
