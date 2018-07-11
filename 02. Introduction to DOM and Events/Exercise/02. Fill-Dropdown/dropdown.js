b20f9313a9699e8cc01ab42bc9147f0559f51ce1
MartinBG <MartinBG@abv.bg>
2018-07-12 00:21:48 +0300

    solution for exercise task #2

--- 02. Introduction to DOM and Events/Exercise/02. Fill-Dropdown/dropdown.js
+++ 02. Introduction to DOM and Events/Exercise/02. Fill-Dropdown/dropdown.js
@@ -1,3 +1,14 @@
 function addItem() {
-    // TODO:
+    var regex1 = new RegExp("^\s*$");
+    let itemText = document.getElementById("newItemText").value;
+    let itemValue = document.getElementById("newItemValue").value;
+    if (regex1.test(itemText) || regex1.test(itemValue)) {
+        return;
+    }
+    document.getElementById("newItemText").value = "";
+    document.getElementById("newItemValue").value = "";
+    let option = document.createElement("option");
+    option.value = itemValue;
+    option.textContent = itemText;
+    document.getElementById("menu").appendChild(option);
 }
