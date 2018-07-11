c711cfa0fe8dbf42ed118bc66cfb94a697554e6e
MartinBG <MartinBG@abv.bg>
2018-07-11 14:20:15 +0300

    Added another selector type as example

--- 02. Introduction to DOM and Events/Lab/01. Sum-Numbers/sum-numbers.js
+++ 02. Introduction to DOM and Events/Lab/01. Sum-Numbers/sum-numbers.js
@@ -3,4 +3,5 @@ function calc() {
     let num2 = Number(document.getElementById("num2").value);
     document.getElementById("sum").value = num1 + num2;
     // document.getElementsByTagName("input")[2].value = num1 + num2;
+    // document.querySelectorAll("input")[2].value = num1 + num2;
 }
