3e4d5f64ad85991994b2d0a3d3d180c3207673dc
MartinBG <MartinBG@abv.bg>
2018-07-12 00:43:31 +0300

    solution for exercise task #4

--- 02. Introduction to DOM and Events/Exercise/04. Sections/sections.js
+++ 02. Introduction to DOM and Events/Exercise/04. Sections/sections.js
@@ -1,3 +1,15 @@
 function create(sentences) {
-    // TODO:
+    for (let sentence of sentences) {
+        let div = document.createElement("div");
+        let p = document.createElement("p");
+        p.innerHTML = sentence;
+        p.style.display = "none";
+        div.appendChild(p);
+        div.addEventListener("click", show);
+        document.getElementById("content").appendChild(div);
+
+        function show() {
+            this.firstChild.style.display = "block";
+        }
+    }
 }
