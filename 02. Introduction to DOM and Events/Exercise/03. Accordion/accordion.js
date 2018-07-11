7b1a6eb7e6c4b5a90284f0fcb59e26139aa6fa99
MartinBG <MartinBG@abv.bg>
2018-07-12 00:28:48 +0300

    solution for exercise task #3

--- 02. Introduction to DOM and Events/Exercise/03. Accordion/accordion.js
+++ 02. Introduction to DOM and Events/Exercise/03. Accordion/accordion.js
@@ -1,3 +1,10 @@
 function toggle() {
-     // TODO:
+    let state = document.getElementsByClassName("button")[0].innerHTML;
+    if (state === "More") {
+        document.getElementsByClassName("button")[0].innerHTML = "Less";
+        document.getElementById("extra").style.display = "block";
+    } else {
+        document.getElementsByClassName("button")[0].innerHTML = "More";
+        document.getElementById("extra").style.display = "none";
+    }
 }
