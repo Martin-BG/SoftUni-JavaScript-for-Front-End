9d6464aa0e2ec0ca8b7bc38b0851a706648a60d0
MartinBG <MartinBG@abv.bg>
2018-07-11 18:40:51 +0300

    solution for lab task #5

--- 02. Introduction to DOM and Events/Lab/05. Add-Delete/add-delete.js
+++ 02. Introduction to DOM and Events/Lab/05. Add-Delete/add-delete.js
@@ -1,3 +1,17 @@
 function addItem() {
-    // TODO:
+    let text = document.getElementById('newText').value;
+    let li = document.createElement("li");
+    li.appendChild(document.createTextNode(text + " "));
+    let span = document.createElement('span');
+    span.innerHTML = "<a href='#'>[Delete]</a>";
+    span.firstChild.addEventListener('click', deleteItem);
+    li.appendChild(span);
+    document.getElementById("items").appendChild(li);
+    document.getElementById('newText').value = '';
+
+    function deleteItem() {
+        let li = this.parentNode.parentNode;
+        let ul = li.parentNode;
+        ul.removeChild(li);
+    }
 }
