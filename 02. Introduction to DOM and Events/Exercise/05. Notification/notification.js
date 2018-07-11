6cdb07efde77f2a3c6c59db9dcb1516ffe5ca68e
MartinBG <MartinBG@abv.bg>
2018-07-12 01:55:23 +0300

    solution for exercise task #5

--- 02. Introduction to DOM and Events/Exercise/05. Notification/notification.js
+++ 02. Introduction to DOM and Events/Exercise/05. Notification/notification.js
@@ -1,6 +1,6 @@
 function notify(message) {
     let notification = document.getElementById("notification");
-    notification.innerText = message;
+    notification.textContent = message;
     notification.style.display = "block";
     setTimeout(function () {
         notification.style.display = "none";
