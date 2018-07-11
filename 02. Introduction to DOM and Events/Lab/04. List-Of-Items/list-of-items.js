782713675f50f5b5ff2719173503205654613b81
MartinBG <MartinBG@abv.bg>
2018-07-11 15:37:27 +0300

    solution for lab task #4

--- 02. Introduction to DOM and Events/Lab/04. List-Of-Items/list-of-items.js
+++ 02. Introduction to DOM and Events/Lab/04. List-Of-Items/list-of-items.js
@@ -1,3 +1,17 @@
 function addItem() {
-    // TODO:
+    let input = document.getElementById('newItemText');
+    let newElementName = input.value;
+    input.value = '';
+    let newListItem = document.createElement('li');
+    newListItem.textContent = newElementName;
+    let list = document.getElementById('items');
+    list.appendChild(newListItem);
+}
+
+function addItemSolutionTwo() {
+    let text = document.getElementById('newItemText').value;
+    let li = document.createElement("li");
+    li.appendChild(document.createTextNode(text));
+    document.getElementById("items").appendChild(li);
+    document.getElementById('newItemText').value = '';
 }
