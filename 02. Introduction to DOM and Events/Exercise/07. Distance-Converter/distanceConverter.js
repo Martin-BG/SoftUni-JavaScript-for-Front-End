e2be1f791039267aee7d9caa45aa34ff33167f19
MartinBG <MartinBG@abv.bg>
2018-07-12 11:08:55 +0300

    solutions

--- 02. Introduction to DOM and Events/Exercise/07. Distance-Converter/distanceConverter.js
+++ 02. Introduction to DOM and Events/Exercise/07. Distance-Converter/distanceConverter.js
@@ -53,3 +53,26 @@ function attachEventsListeners() {
         }
     }
 }
+
+// Credits to kostovhg:
+// https:github.com/kostovhg/SoftUni/blob/master/FrontEnd/JavaScriptForFrontEnd/d_DOMAndEvents/g_DistanceConverter/distanceConverter.js
+function betterSolution() {
+
+    let units = {
+        'km': 1000, 'm': 1, 'cm': 0.01, 'mm': 0.001,
+        'mi': 1609.34, 'yrd': 0.9144, 'ft': 0.3048, 'in': 0.0254,
+    };
+
+    function attachEvent() {
+        let [fromUnits, toUnits] = [
+            document.getElementById('inputUnits'),
+            document.getElementById('outputUnits'),
+        ];
+        document.getElementById('convert').addEventListener('click', function () {
+            document.getElementById('outputDistance').value =
+                document.getElementById('inputDistance').value * units[fromUnits.value] / units[toUnits.value];
+        });
+    }
+
+    return attachEvent();
+}
--- 02. Introduction to DOM and Events/Lab/06. Stopwatch/stopwatch.js
+++ 02. Introduction to DOM and Events/Lab/06. Stopwatch/stopwatch.js
@@ -1,3 +1,43 @@
+// Credits to kostovhg:
+// https:github.com/kostovhg/SoftUni/blob/master/FrontEnd/JavaScriptForFrontEnd/d_DOMAndEvents/g_DistanceConverter/distanceConverter.js
 function stopwatch() {
-    // TODO:
+    let [time, stopBtn, startBtn, seconds, isRunning] =
+        [
+            document.getElementById('time'),
+            document.getElementById('stopBtn'),
+            document.getElementById('startBtn'),
+            0,
+            false
+        ];
+    let interval;
+
+    startBtn.addEventListener('click', startTimer);
+    stopBtn.addEventListener('click', stopTimer);
+
+    function update() {
+        let min = ~~(seconds / 60);
+        if (min >= 60) min = 0;
+        time.textContent = `${("0" + min).slice(-2)}:${("0" + (seconds % 60)).slice(-2)}`;
+        seconds++;
+    }
+
+    function startTimer() {
+        startBtn.setAttribute('disabled', 'true');
+        stopBtn.removeAttribute('disabled');
+        if (!isRunning) {
+            update();
+            interval = setInterval(update, 1000);
+            isRunning = true;
+        }
+    }
+
+    function stopTimer() {
+        startBtn.removeAttribute('disabled');
+        stopBtn.setAttribute('disabled', 'true');
+        if (isRunning) {
+            clearInterval(interval);
+            isRunning = false;
+            seconds = 0;
+        }
+    }
 }
