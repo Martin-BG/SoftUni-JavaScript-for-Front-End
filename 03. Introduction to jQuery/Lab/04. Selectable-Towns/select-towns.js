517d27b5b488934af25aa95a2fb8c258a278186b
MartinBG <MartinBG@abv.bg>
2018-07-12 21:41:55 +0300

    solutions for lab #4

--- 03. Introduction to jQuery/Lab/04. Selectable-Towns/select-towns.js
+++ 03. Introduction to jQuery/Lab/04. Selectable-Towns/select-towns.js
@@ -1,3 +1,25 @@
 function attachEvents() {
-	// TODO:
+    $("ul").on("click", "li", onListItemClick);
+    $("#showTownsButton").on("click", onShowTownsClick);
+
+    function onShowTownsClick() {
+        $("#selectedTowns").text(
+            $("#items li")
+                .toArray()
+                .filter(li => typeof $(li).attr("data-selected") !== typeof undefined && $(li).attr("data-selected") === "true")
+                .map(li => li.textContent)
+                .join(", ")
+        );
+    }
+
+    function onListItemClick() {
+        let attr = $(this).attr("data-selected");
+        if (typeof attr !== typeof undefined && attr === "true") {
+            $(this).removeAttr("data-selected");
+            $(this).css("color", "");
+        } else {
+            $(this).attr("data-selected", "true");
+            $(this).css("color", "#DDD");
+        }
+    }
 }
