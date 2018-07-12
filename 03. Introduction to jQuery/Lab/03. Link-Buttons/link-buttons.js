30174bf789392c57aec26f7975560b26643e8c87
MartinBG <MartinBG@abv.bg>
2018-07-12 20:44:11 +0300

    solutions for lab #3

--- 03. Introduction to jQuery/Lab/03. Link-Buttons/link-buttons.js
+++ 03. Introduction to jQuery/Lab/03. Link-Buttons/link-buttons.js
@@ -1,3 +1,21 @@
 function attachEvents() {
-   // TODO:
+    $("a.button").on('click', buttonClicked);
+
+    function buttonClicked() {
+        if ($(this).hasClass("selected")) {
+            $(this).removeClass("selected");
+        } else {
+            $(".selected").removeClass("selected");
+            $(this).addClass("selected");
+        }
+    }
+}
+
+function attachEventsOther() {
+    $('a.button').on('click', buttonClicked);
+
+    function buttonClicked() {
+        $('.selected').removeClass('selected');
+        $(this).addClass('selected');
+    }
 }
