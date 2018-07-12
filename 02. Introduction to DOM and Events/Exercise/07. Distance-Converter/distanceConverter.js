3ff9f9462816709c3832a2284904b27bc415a255
MartinBG <MartinBG@abv.bg>
2018-07-12 03:29:02 +0300

    solution for exercise task #7

--- 02. Introduction to DOM and Events/Exercise/07. Distance-Converter/distanceConverter.js
+++ 02. Introduction to DOM and Events/Exercise/07. Distance-Converter/distanceConverter.js
@@ -1,3 +1,55 @@
 function attachEventsListeners() {
-    // TODO:
+    let button = document.getElementById("convert");
+    button.addEventListener("click", convertDistance);
+
+    function convertDistance() {
+        let inputDist = Number(document.getElementById("inputDistance").value);
+        let inputUnit = document.getElementById("inputUnits").value;
+        let outputUnit = document.getElementById("outputUnits").value;
+        let outputDist = convFromM(convToM(inputDist, inputUnit), outputUnit);
+        document.getElementById("outputDistance").value = outputDist;
+        document.getElementById("inputDistance").value = inputDist;
+    }
+
+    function convToM(value, unit) {
+        switch (unit) {
+            case "km":
+                return value * 1000;
+            case "m":
+                return value;
+            case "cm":
+                return value * 0.01;
+            case "mm":
+                return value * 0.001;
+            case "mi":
+                return value * 1609.34;
+            case "yrd":
+                return value * 0.9144;
+            case "ft":
+                return value * 0.3048;
+            case "in":
+                return value * 0.0254;
+        }
+    }
+
+    function convFromM(value, unit) {
+        switch (unit) {
+            case "km":
+                return value / 1000;
+            case "m":
+                return value;
+            case "cm":
+                return value / 0.01;
+            case "mm":
+                return value / 0.001;
+            case "mi":
+                return value / 1609.34;
+            case "yrd":
+                return value / 0.9144;
+            case "ft":
+                return value / 0.3048;
+            case "in":
+                return value / 0.0254;
+        }
+    }
 }
