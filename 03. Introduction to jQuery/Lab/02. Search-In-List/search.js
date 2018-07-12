71a5649874ad5eeafbe1962e8e8d822b32b3f5cc
MartinBG <MartinBG@abv.bg>
2018-07-12 20:36:33 +0300

    solutions for lab #2

--- 03. Introduction to jQuery/Lab/02. Search-In-List/search.js
+++ 03. Introduction to jQuery/Lab/02. Search-In-List/search.js
@@ -1,3 +1,24 @@
 function search() {
-   // TODO:
+    $("#towns li").css("font-weight", "");
+    let searchText = $("#searchText").val();
+    let matches = 0;
+    $("#towns li:contains('" + searchText + "')")
+        .each((index, item) => {
+            $(item).css("font-weight", "bold");
+            matches++;
+        });
+    $("#result").text(`${matches} matches found.`);
+}
+
+function searchAnother() {
+    let searchText = $('#searchText').val();
+    let matches = 0;
+    $("#towns li").each((index, item) => {
+        if (item.textContent.includes(searchText)) {
+            $(item).css("font-weight", "bold");
+            matches++;
+        } else
+            $(item).css("font-weight", "");
+    });
+    $('#result').text(matches + " matches found.");
 }
