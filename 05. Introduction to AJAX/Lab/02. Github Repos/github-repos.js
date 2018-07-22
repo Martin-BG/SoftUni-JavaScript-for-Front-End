00bbe275714a54019bdca5ddc43ca6747abad766
MartinBG <MartinBG@abv.bg>
2018-07-23 00:56:05 +0300

    Solution for task #2

---
+++ 05. Introduction to AJAX/Lab/02. Github Repos/github-repos.js
@@ -0,0 +1,25 @@
+function loadRepos() {
+    $("#repos").empty();
+    let url = "https://api.github.com/users/" +
+        $("#username").val() + "/repos";
+    $.ajax({
+        url: url,
+        success: displayRepos,
+        error: displayError
+    });
+
+    function displayRepos(respos) {
+        for (let repo of respos) {
+            $("#repos")
+                .append($('<li>')
+                    .append($('<a>')
+                        .text(repo.full_name)
+                        .attr('href', repo.html_url)));
+        }
+    }
+
+    function displayError(err) {
+        $("#repos")
+            .append($("<li>Error</li>"));
+    }
+}
