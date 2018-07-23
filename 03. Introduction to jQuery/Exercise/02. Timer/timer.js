94f83ace55cb9b23bb5d9f590984d9c4b2597778
MartinBG <MartinBG@abv.bg>
2018-07-23 04:34:17 +0300

    Solution for task #2

--- 03. Introduction to jQuery/Exercise/02. Timer/timer.js
+++ 03. Introduction to jQuery/Exercise/02. Timer/timer.js
@@ -1,3 +1,26 @@
 function timer() {
-   // TODO:
+    let [timeInSeconds, hours, minutes, seconds, start, stop] = [
+        0, $(`#hours`), $(`#minutes`), $(`#seconds`), $(`#start-timer`), $(`#stop-timer`)
+    ];
+
+
+    let interv;
+
+    start.on("click", function () {
+        if (!interv) {
+            interv = setInterval(() => {
+                timeInSeconds = Number(hours.text()) * 3600 + Number(minutes.text()) * 60 + Number(seconds.text());
+                timeInSeconds++;
+                hours.text(("0" + Math.floor(timeInSeconds / 3600)).slice(-2));
+                minutes.text(("0" + Math.floor((timeInSeconds % 3600) / 60)).slice(-2));
+                seconds.text(("0" + Math.floor(timeInSeconds % 60)).slice(-2));
+            }, 1000);
+        }
+    });
+    stop.on("click", function () {
+        if (interv) {
+            clearInterval(interv);
+            interv = null;
+        }
+    });
 }
