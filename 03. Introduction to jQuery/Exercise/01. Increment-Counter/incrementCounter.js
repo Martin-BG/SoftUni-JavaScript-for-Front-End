e34daef7084f8ffd7d81fb0bf233d17f0a1a645a
MartinBG <MartinBG@abv.bg>
2018-07-23 03:36:04 +0300

    Solution for task #1

--- 03. Introduction to jQuery/Exercise/01. Increment-Counter/incrementCounter.js
+++ 03. Introduction to jQuery/Exercise/01. Increment-Counter/incrementCounter.js
@@ -1,3 +1,30 @@
 function increment(selector) {
-    // TODO: 
+    let container = $(selector);
+    let fragment = document.createDocumentFragment();
+    let textArea = $("<textarea>")
+        .val(0)
+        .addClass("counter")
+        .attr("disabled", true);
+    let incrementBtn = $("<button>Increment</button>")
+        .addClass("btn")
+        .attr("id", "incrementBtn");
+    let addBtn = $("<button>Add</button>").addClass("btn")
+        .attr("id", "addBtn");
+    let list = $('<ul>')
+        .addClass("results");
+
+    textArea.appendTo(fragment);
+    incrementBtn.appendTo(fragment);
+    addBtn.appendTo(fragment);
+    list.appendTo(fragment);
+
+    container.append(fragment);
+
+    $(incrementBtn).on("click", function () {
+        textArea.val(+textArea.val() + 1);
+    });
+
+    $(addBtn).on("click", function () {
+        list.append($(`<li>${textArea.val()}</li>`));
+    });
 }
