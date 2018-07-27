40195503c3ae5e4206b87ee2a31e0be8b80a9992
MartinBG <MartinBG@abv.bg>
2018-07-27 22:56:13 +0300

    Implemented text bold/italic functions

--- 04. jQuery Plug-ins/Exercise/03. Form-Validator/app.js
+++ 04. jQuery Plug-ins/Exercise/03. Form-Validator/app.js
@@ -1,3 +1,11 @@
 $.validate({
     modules: 'security'
 });
+
+$('#bold-btn').on('click', () => {
+    $('#format-text input').toggleClass('text-bold')
+});
+
+$('#italic-btn').on('click', () => {
+    $('#format-text input').toggleClass('text-italic')
+});
