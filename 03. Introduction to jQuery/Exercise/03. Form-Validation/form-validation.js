9fc913b0b6d4b094e35df91cd24945c7ce50f40f
MartinBG <MartinBG@abv.bg>
2018-09-18 01:18:27 +0300

    a solution

--- 03. Introduction to jQuery/Exercise/03. Form-Validation/form-validation.js
+++ 03. Introduction to jQuery/Exercise/03. Form-Validation/form-validation.js
@@ -1,3 +1,47 @@
 function validate() {
-	// TODO:
+    //TODO - refactor
+    $("#companyInfo").css("display", "none");
+    let submit = $("#submit");
+    let company = $("#company");
+    let companyInfo = $("#companyInfo");
+    let valid = $("#valid");
+    let isCompany = () => company.is(":checked");
+    let format = (field, v) => (!v) ? field.css("border", "2px solid red") : field.css("border", "none");
+    let inputs = $(".pairContainer input").toArray().map(i => $(i).attr("id"));
+
+
+    submit.on("click", function (e) {
+        e.preventDefault();
+        validateForm();
+    });
+
+    companyInfo.css("display", "table");
+
+    let validations = {
+        username: (v) => (/^[a-zA-Z0-9]{3,20}$/g).test(v),
+        password: (v) => (/^\w{5,15}$/g).test(v) && $("#confirm-password").val() === v,
+        "confirm-password": (v) => validations.password(v),
+        email: (v) => (/^.*?@.*?\..*$/g).test(v),
+        company: (v) => true,
+        companyNumber: (v) => isCompany() ? (v >= 1000 && v <= 9999) : true,
+    };
+
+    function validateForm() {
+        let formValidity = true;
+        for (let input of inputs) {
+            let field = $("input[id=${input}]");
+            let fieldValidity = validations[input](field.val());
+            format(field, fieldValidity);
+            formValidity &= fieldValidity;
+        }
+        (formValidity) ? valid.css("display", "table") : valid.css("display", "none");
+    }
+
+    company.change(() => {
+        if (company.is(":checked")) {
+            $("#companyInfo").css("display", "block");
+        } else {
+            $("#companyInfo").css("display", "none");
+        }
+    });
 }
